654e34b53d83403f585db6668c28ed76b8ea4234
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-07-26 20:01:36 +0300

    initial: create, read

---
+++ index.js
@@ -0,0 +1,32 @@
+const _data = require('./lib/data');
+
+_data.read('users', 'petras', (err, data) => {
+    if (err || !data) {
+        console.log('Nepavyko perskaityti failo...');
+        return false;
+    }
+
+    const obj = JSON.parse(data);
+    console.log(obj);
+})
+
+
+const pazymiai = [10, 2, 8, 4, 6];
+_data.create('marks', 'leburda', pazymiai, (err) => {
+    if (err) {
+        console.log(err);
+        return false;
+    }
+
+    console.log('Failas sekmingai sukurtas!');
+
+    _data.read('marks', 'leburda', (err, data) => {
+        if (err || !data) {
+            console.log('Nepavyko perskaityti Kazio failo...');
+            return false;
+        }
+
+        const obj = JSON.parse(data);
+        console.log(obj);
+    })
+});
