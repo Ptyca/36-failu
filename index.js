56ae5d674630607ff27c26d4a19c2a956082c239
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-07-28 18:28:08 +0300

    delete

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
+const { log } = require('console');
 const _data = require('./lib/data');
 
-_data.read('users', 'petras', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
-        return false;
-    }
+// _data.read('users', 'petras', (err, data) => {
+//     if (err || !data) {
+//         console.log('Nepavyko perskaityti failo...');
+//         return false;
+//     }
 
-    const obj = JSON.parse(data);
-    console.log(obj);
-})
+//     const obj = JSON.parse(data);
+//     console.log(obj);
+// })
 
 
-const pazymiai = [10, 2, 8, 4, 6];
-_data.create('marks', 'leburda', pazymiai, (err) => {
+// const pazymiai = [10, 2, 8, 4, 6];
+// _data.create('marks', 'kazys', pazymiai, (err) => {
+//     if (err) {
+//         console.log(err);
+//         return false;
+//     }
+
+//     console.log('Failas sekmingai sukurtas!');
+
+//     _data.read('marks', 'kazys', (err, data) => {
+//         if (err || !data) {
+//             console.log('Nepavyko perskaityti Kazio failo...');
+//             return false;
+//         }
+
+//         const obj = JSON.parse(data);
+//         console.log(obj);
+//     })
+// });
+
+// const petras = {
+//     name: 'Petras',
+//     age: 99,
+//     car: 'Audi',
+//     color: 'red'
+// }
+// _data.update('users', 'petras', petras, (err) => {
+//     if (err) {
+//         console.log(err);
+//         return false;
+//     }
+
+//     console.log('Petro duomenys atnaujinti sekmingai.');
+
+//     _data.read('users', 'petras', (err, data) => {
+//         if (err || !data) {
+//             console.log('Nepavyko perskaityti Petro failo...');
+//             return false;
+//         }
+
+//         const obj = JSON.parse(data);
+//         console.log(obj);
+//     })
+// });
+
+_data.delete('users', 'maryte', (err) => {
     if (err) {
         console.log(err);
         return false;
     }
 
-    console.log('Failas sekmingai sukurtas!');
-
-    _data.read('marks', 'leburda', (err, data) => {
-        if (err || !data) {
-            console.log('Nepavyko perskaityti Kazio failo...');
-            return false;
-        }
-
-        const obj = JSON.parse(data);
-        console.log(obj);
-    })
-});
+    console.log('Failas sekmingai istrintas.');
+})
